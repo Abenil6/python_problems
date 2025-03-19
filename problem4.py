290f3afb5b300dbf5ff7b3883d1e29059360ab02
Abenil6 <abenilee740@gmail.com>
2025-03-19 12:04:40 +0300

    Problem 4 Solved

---
+++ problem4.py
@@ -0,0 +1,10 @@
+
+x = 10
+y = 3.14
+z = "HUCISA"
+a = True
+
+print(f"the data type of x is {type(x)}")
+print(f"the data type of y is {type(y)}")
+print(f"the data type of z is {type(z)}")
+print(f"the data type of a is {type(a)}")
